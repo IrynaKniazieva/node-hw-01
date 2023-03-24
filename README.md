2a997020e877b3c2b314f37957404cb5885cae72
IrynaKniazieva <gleb0812@gmail.com>
2023-03-24 21:46:12 +0200

    work

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# node-hw-01
+Получаем и выводим весь список контактов в виде таблицы: https://ibb.co/BB02h6s
+Получаем контакт по id: https://ibb.co/XjpNbnV
+Добавялем контакт: https://ibb.co/hy67LR1
+Удаляем контакт: https://ibb.co/2ytfGCw
---
+++ index.js
@@ -0,0 +1,37 @@
+const contacts = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async({action, id, name, email, phone}) => {
+    switch(action) {
+        case "list":
+            const allContacts = await contacts.listContacts();
+            return console.table(allContacts);
+        case "get":
+            const oneContact = await contacts.getContactById(id);
+            return console.log(oneContact);
+        case "add":
+            const newContact = await contacts.addContact({name, email, phone});
+            return console.log(newContact);
+        case "remove":
+            const deleteContact = await contacts.removeContact(id);
+            return console.log(deleteContact);
+
+    }
+}
+invokeAction(argv);
+
+// invokeAction({action: "list"})  
+// invokeAction({action: "get", id: "drsAJ4SHPYqZeG-83QTVW"}) 
+// invokeAction({action: "add", name: "Ira", email: "ira@mail.com", phone: "98765432"})
+// invokeAction({action: "delete", id: "Oqq2GMUkqQ8O9BXamx47J"});
